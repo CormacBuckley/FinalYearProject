072ceac0a3bd518c9b6210ae7790dda7f4ca9706
Cormac Buckley <cormacbuckley@Cormacs-MacBook-Pro.local>
2019-01-21 17:31:18 +0000

    More work on prediction from guide

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-#Data Preprocessing
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, array_to_img, image
 
 datagen = ImageDataGenerator(
@@ -36,10 +35,8 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# the model so far outputs 3D feature maps (height, width, features)
 
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Flatten()) 
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
@@ -53,7 +50,6 @@ model.compile(loss='binary_crossentropy',
 
 batch_size=16
 
-#Augmentation Config
 train_datagen=ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -61,21 +57,14 @@ train_datagen=ImageDataGenerator(
     horizontal_flip=True
 )
 
-
-# this is the augmentation configuration we will use for testing:
-# only rescaling
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-# this is a generator that will read pictures found in
-# subfolers of 'data/train', and indefinitely generate
-# batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
-        '../TrainingData',  # this is the target directory
-        target_size=(150, 150),  # all images will be resized to 150x150
+        '../TrainingData',  
+        target_size=(150, 150),  
         batch_size=batch_size,
-        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')  
 
-# this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
         '../TestingData',
         target_size=(150, 150),
@@ -85,31 +74,28 @@ validation_generator = test_datagen.flow_from_directory(
 model.fit_generator(
         train_generator,
         steps_per_epoch=500 // batch_size,
-        epochs=5,
+        epochs=1,
         validation_data=validation_generator,
         validation_steps=200 // batch_size)
-
-
-
-#Display Data
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-
 img1 = image.load_img("../TrainingData/Empty/2013-01-17_10_40_06#006.jpg", target_size=(150, 150))
 img2 = image.load_img("../TrainingData/Empty/2013-01-17_10_40_06#007.jpg", target_size=(150, 150))
 img3 = image.load_img("../TrainingData/Occupied/2013-01-17_10_40_06#001.jpg", target_size=(150, 150))
+img4 = image.load_img("../TestingData/2013-02-28_18_40_45#031.jpg", target_size=(150, 150))
 
-images = [img1,img2,img3]
 
-targets=[img1,img2,img3]
+images = [img1,img2,img3,img4]
+
+targets = []
 
 plt.figure(1)
 
 for i in range(len(images)):
-
+    targets.append(images[i])
     targets[i] = np.expand_dims(img_to_array(targets[i]), axis=0)
 
     result = model.predict(targets[i])
@@ -121,7 +107,7 @@ for i in range(len(images)):
 
     print(prediction)
     print(result[0][0])
-    ax1 = plt.subplot(1,3,i+1)
+    ax1 = plt.subplot(1,len(images),i+1)
     plt.imshow(images[i])
     ax1.title.set_text(prediction)
     plt.axis('off')
